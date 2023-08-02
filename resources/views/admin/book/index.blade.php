3d93c0a8ed36b92ff3c851b6db3482730192f9f0
kingkazuma7 <your.nasionasio7@gmail.com>
2023-08-02 19:42:34 +0900

    view blade

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -20,7 +20,7 @@
                 <th>価格</th>
             </tr>
             @foreach ($books as $book)
-                <tr>
+                <tr @if ($loop->even) style="background-color:#E0E0E0" @endif></tr>
                     <td>{{ $book->category->title }}</td>
                     <td>{{ $book->title }}</td>
                     <td>{{ $book->price }}</td>
